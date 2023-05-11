ae9a073514b8c7ef822b7a899b0a492ad7a74464
ushiostarfish <ushiostarfish@gmail.com>
2023-05-11 22:12:16 +0900

    Batch draw

--- main.cpp
+++ main.cpp
@@ -241,6 +241,44 @@ float minElement(float a, float b, float c)
     return glm::min(glm::min(a, b), c);
 }
 
+inline void drawVoxels(const std::set<uint64_t>& mortonVoxels, const glm::vec3& origin, float dps, glm::u8vec3 color )
+{
+    using namespace pr;
+
+    PrimBegin(PrimitiveMode::Lines);
+    for (auto morton : mortonVoxels)
+    {
+        glm::uvec3 c;
+        decodeMortonCode_PEXT(morton, &c.x, &c.y, &c.z);
+        glm::vec3 p = origin + glm::vec3(c.x, c.y, c.z) * dps;
+
+        uint32_t i0 = PrimVertex(p, color);
+        uint32_t i1 = PrimVertex(p + glm::vec3(dps, 0, 0), color);
+        uint32_t i2 = PrimVertex(p + glm::vec3(dps, 0, dps), color);
+        uint32_t i3 = PrimVertex(p + glm::vec3(0, 0, dps), color);
+        uint32_t i4 = PrimVertex(p + glm::vec3(0, dps, 0), color);
+        uint32_t i5 = PrimVertex(p + glm::vec3(dps, dps, 0), color);
+        uint32_t i6 = PrimVertex(p + glm::vec3(dps, dps, dps), color);
+        uint32_t i7 = PrimVertex(p + glm::vec3(0, dps, dps), color);
+
+        PrimIndex(i0); PrimIndex(i1);
+        PrimIndex(i1); PrimIndex(i2);
+        PrimIndex(i2); PrimIndex(i3);
+        PrimIndex(i3); PrimIndex(i0);
+
+        PrimIndex(i4); PrimIndex(i5);
+        PrimIndex(i5); PrimIndex(i6);
+        PrimIndex(i6); PrimIndex(i7);
+        PrimIndex(i7); PrimIndex(i4);
+
+        PrimIndex(i0); PrimIndex(i4);
+        PrimIndex(i1); PrimIndex(i5);
+        PrimIndex(i2); PrimIndex(i6);
+        PrimIndex(i3); PrimIndex(i7);
+    }
+    PrimEnd();
+}
+
 inline void buildOctree( std::vector<OctreeNode>* nodes, const std::set<uint64_t>& mortonVoxels, int wide )
 {
     nodes->clear();
@@ -627,13 +665,7 @@ int main() {
             // Draw
             if( drawVoxelWire )
             {
-                for( auto morton : mortonVoxels )
-                {
-                    glm::uvec3 c;
-                    decodeMortonCode_PEXT( morton, &c.x, &c.y, &c.z );
-                    glm::vec3 p = origin + glm::vec3(c.x, c.y, c.z) * dps;
-                    DrawAABB(p, p + glm::vec3(dps, dps, dps), { 200 ,200 ,200 });
-                }
+                drawVoxels(mortonVoxels, origin, dps, { 200 ,200 ,200 });
             }
 
             // voxel build
